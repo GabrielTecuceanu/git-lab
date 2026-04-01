6263003f3666828edeec0831a3cf93834f8708ce
Gabriel-Cristian Tecuceanu <tecuceanu.gabriel22@gmail.com>
2026-04-01 07:51:23 +0300

    Fix bad
    Signed-off-by: Gabriel-Cristian Tecuceanu <tecuceanu.gabriel22@gmail.com>

--- practice/main.cc
+++ practice/main.cc
@@ -1,90 +1,66 @@
-#include <iostream>
-#include <cstdlib>
 #include <cstdio>
+#include <cstdlib>
 #include <immintrin.h>
-#include <vector>
+#include <iostream>
 #include <sys/random.h>
+#include <vector>
 
 using namespace std;
 
-int get_random_std()
-{
-    return rand() % 200;
-}
+int get_random_std() { return rand() % 200; }
 
 int get_random_rdrand()
 {
-    unsigned int ret;
+	unsigned int ret;
 
-    int rc = _rdrand32_step(&ret);
+	int rc = _rdrand32_step(&ret);
 
-    if (rc == 1)
-        return ret;
-    else
-        return -1;
+	if (rc == 1)
+		return ret;
+	else
+		return -1;
 }
 
 int get_random_dev_urandom()
 {
-    int ret, read;
+	int ret, read;
 
-    FILE *urandom = fopen("/dev/urandom", "r");
+	FILE *urandom = fopen("/dev/urandom", "r");
 
-    read = fread(&ret, sizeof(ret), 1, urandom);
+	read = fread(&ret, sizeof(ret), 1, urandom);
 
-    fclose(urandom);
+	fclose(urandom);
 
-    if (read > 0)
-        return ret;
-    else
-        return -1;
+	if (read > 0)
+		return ret;
+	else
+		return -1;
 }
 
 int get_random_getrandom()
 {
-    int ret, read;
+	int ret, read;
 
-    read = getrandom(&ret, sizeof(ret), 0);
+	read = getrandom(&ret, sizeof(ret), 0);
 
-    if (read > 0)
-        return ret;
-    else
-        return read;
+	if (read > 0)
+		return ret;
+	else
+		return read;
 }
 
-int get_random(int (*random_func)())
-{
-    return random_func() % 200;
-}
+int get_random(int (*random_func)()) { return random_func() % 200; }
 
 typedef int (*fp)();
 
 int main()
 {
-    int n;
+	int n;
 
-    cout << "Please enter a number" << endl;
-    cin >> n;
+	cout << "Please enter a number" << endl;
+	cin >> n;
 
-    uint32_t rnd;
+	cout << "Your number + 5 is: " << n + 5 << endl;
 
-    srand((unsigned) time(NULL));
-
-    vector<fp> randoms = { get_random_getrandom, get_random_dev_urandom, get_random_rdrand, get_random_std };
-
-    for (int i = 0; i <= 5; i++)
-    {
-        if (get_random(randoms[i % 4]) == 150)
-        {
-            n += 1;
-        }
-        else
-        {
-            i--;
-        }
-    }
-
-    cout << "Your number + 5 is: " << n << endl;
-
-    return 0;
+	return 0;
 }
